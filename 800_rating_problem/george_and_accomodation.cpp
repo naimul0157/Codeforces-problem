d7a4c81d45431e2d2504d29cf7ec39e7c7c8c1b0
naimul0157 <naimul794@gmail.com>
2026-01-07 00:34:53 +0600

    second commit

---
+++ 800_rating_problem/george_and_accomodation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int i, j;
+    for (int i = 1; i <=n; i++)
+    {
+        cin>>i>>j;
+    }
+    cout<<"hello";
+    return 0;
+}
