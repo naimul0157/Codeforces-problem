dff55ec5e4742c5e081962bce302405b123b485d
naimul0157 <naimul794@gmail.com>
2026-01-09 19:51:01 +0600

    namata calculator

---
+++ 800_rating_problem/Namta.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin>>n;
+    for (int i = 1; i < 11; i++)
+    {
+        cout<<n<<"x"<<i<<"="<<n*i<<endl;
+    }
+    
+    return 0;
+}
